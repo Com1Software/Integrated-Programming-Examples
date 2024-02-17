e06885a792ce508f1dbf161b819d4bb390018da7
Com1 Software, Inc <information@com1software.com>
2024-02-17 09:51:17 -0500

    Create main.go

---
+++ Go calling Python pyCode/main.go
@@ -0,0 +1,25 @@
+ 1 package main
+ 2 
+ 3 // #cgo pkg-config: python3
+ 4 // #include <Python.h>
+ 5 import "C"
+ 6
+ 7 import (
+ 8   "unsafe"
+ 9 )
+10 
+11 func main() {
+12
+13   pycodeGo := `
+14 import sys
+15 for path in sys.path:
+16   print(path)
+17 ` 
+18   
+19   defer C.Py_Finalize()
+20   C.Py_Initialize()
+21   pycodeC := C.CString(pycodeGo)
+22   defer C.free(unsafe.Pointer(pycodeC))
+23   C.PyRun_SimpleString(pycodeC)
+24 
+25 }

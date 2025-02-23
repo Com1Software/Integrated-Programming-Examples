f1919b1d0f2f5ba0f05cc310e8b74c60fbe1d1e4
Lenovo 15 <information@com1software.com>
2025-02-23 15:24:17 -0500

    added

---
+++ Go Calling GUI C/main.go
@@ -0,0 +1,16 @@
+// main.go
+package main
+
+/*
+#include <stdio.h>
+#include <stdlib.h>
+#include "hello.c"
+*/
+import "C"
+import "unsafe"
+
+func main() {
+	name := C.CString("In the Go Program...")
+	defer C.free(unsafe.Pointer(name))
+	C.SayHello(name)
+}
